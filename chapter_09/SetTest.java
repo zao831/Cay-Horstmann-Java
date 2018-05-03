bcdf435bafd99b5b1615e13c58bbc7418545a8fb
zao831 <zao831@gmail.com>
2018-05-03 13:38:16 +0300

    Added SetTest.java to chapter_09 package

---
+++ chapter_09/SetTest.java
@@ -0,0 +1,40 @@
+package chapter_09;
+
+/*
+ * Cay S. Horstmann
+ * Core Java Volume 1. Fundamentals
+ * 10th Edition
+ * Page 439
+ * This program displays all the unique words 
+ * entered into the set from the standard stream
+ */
+
+import java.util.*;
+
+public class SetTest {
+
+	public static void main(String[] args) {
+
+		Set<String> words = new HashSet<>(); // The object of type HashSet implementing a hash set
+
+		long totalTime = 0;
+
+		try (Scanner in = new Scanner(System.in)) {
+			while (in.hasNext()) {
+				String word = in.next();
+				long callTime = System.currentTimeMillis();
+				words.add(word);
+				callTime = System.currentTimeMillis() - callTime;
+				totalTime += callTime;
+			}
+		}
+
+		Iterator<String> iter = words.iterator();
+
+		for (int i = 0; i <= 20 && iter.hasNext(); i++)
+			System.out.println(iter.next());
+		System.out.println("...");
+		System.out.println(words.size() + " distinct words. " + totalTime + " milliseconds. ");
+
+	}
+}
